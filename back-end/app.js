fb0d6d92a4ee377f42ac7676691e83c9bdbc522b
Zeee2000 <u19086688@tuks.co.za>
2021-05-29 16:29:02 +0200

    Created file structure and setup for back-end - Zelealem Tesema

---
+++ back-end/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app= express();
+const morgan= require('morgan');
+const bodyParser= require('body-parser');
+
+
+// This is middleware. incoming requests have to go through the middelware.
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use((req,res,next) =>
+{
+    res.header('Access-Control-Allow', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+
+    if (req.method === 'OPTIONS')
+    {
+        res.header('Access-Control-Allow-Methods' , 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
+
+// Routes which should handle requests
+app.use('/products' , productRoutes);
+app.use('/orders' , orderRoutes);
+
+app.use((req, res, next)=>
+{
+    const error= new Error('Not found');
+    error.status=404;
+    next(error);
+})
+
+app.use((error, req, res,next) =>
+{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+module.exports = app;
