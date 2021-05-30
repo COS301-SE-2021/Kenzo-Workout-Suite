89938a15e3dacee5181fbc9e22e81c75f045a59f
slickjumper <u19050412@tuks.co.za>
2021-05-30 20:41:44 +0200

    added getExerciseByTitle in workoutController - Tinashe Chamisa

--- back-end/api/controllers/workoutController.js
+++ back-end/api/controllers/workoutController.js
@@ -7,15 +7,15 @@ const bcrypt= require('bcrypt');
 exports.getWorkoutByTitle=async (req,res,next) =>
 {
     const workouts = await prisma.workout.findMany({
+        where: {
+            workoutTitle: req.body.title
+        },
         select: {
             workoutTitle: true,
             workoutDescription: true,
-            exercises: true, //STILL NEED TO TEST
+            exercises: true,
             difficulty: true,
             planner_Email: true
-        },
-        where: {
-            title : req.body.title
         }
     });
 
@@ -27,7 +27,28 @@ exports.getWorkoutByTitle=async (req,res,next) =>
 
 exports.getExerciseByTitle=async (req,res,next) =>
 {
+    const workouts = await prisma.exercise.findMany({
+        where: {
+            title : req.body.title
+        },
+        select: {
+            title: true,
+            description: true,
+            repRange: true,
+            sets: true,
+            Posedescription: true,
+            restPeriod: true,
+            wasSkipped: true,
+            difficulty: true,
+            duratime: true,
+            workout_id: true
+        }
+    });
 
+    res.status(200).json({
+        message: "Successfully retrieved exercise",
+        data: workouts
+    });
 }
 
 exports.getWorkoutByPlanner=async (req,res,next) =>
