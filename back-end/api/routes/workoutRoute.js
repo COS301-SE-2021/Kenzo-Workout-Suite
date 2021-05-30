1130bd332b86c89461297af9061551d5ca7ea3c1
slickjumper <u19050412@tuks.co.za>
2021-05-30 18:40:04 +0200

    edited workoutRoute - Tinashe Chamisa

--- back-end/api/routes/workoutRoute.js
+++ back-end/api/routes/workoutRoute.js
@@ -2,10 +2,10 @@ const express = require ('express');
 
 const router = express.Router();
 
-const userController=require("../controllers/workoutController")
+const workoutController = require("../controllers/workoutController")
 
 router.post('/getworkoutbytitle', workoutController.getWorkoutByTitle);
 router.post('/getexercisebytitle', workoutController.getExerciseByTitle);
-router.post('/getworkoutbyplanner', workoutController.getWorkoutByPlanner)
+router.post('/getworkoutbyplanner', workoutController.getWorkoutByPlanner);
 
 module.exports = router;
