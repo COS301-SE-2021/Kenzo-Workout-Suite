70998590fb7c5f8ae1cf3dc31210fb2f00d16ca1
slickjumper <u19050412@tuks.co.za>
2021-05-31 11:19:05 +0200

    swagger draft for getworkoutbytitle in workoutController and package installations - Tinashe Chamisa

--- back-end/api/routes/workoutRoute.js
+++ back-end/api/routes/workoutRoute.js
@@ -4,6 +4,18 @@ const router = express.Router();
 
 const workoutController = require("../controllers/workoutController")
 
+/**
+ * @swagger
+ *  /getworkoutbytitle:
+ *      get:
+ *          description: Gets all the workouts in the database that have the same title provided
+ *          parameters:
+ *          - title: title of the workout
+ *
+ *          responses:
+ *              200:
+ *                  description: Success
+ */
 router.get('/getworkoutbytitle/:title', workoutController.getWorkoutByTitle);
 router.get('/getexercisebytitle/:title', workoutController.getExerciseByTitle);
 router.get('/getworkoutbyplanner/:email', workoutController.getWorkoutByPlanner);
