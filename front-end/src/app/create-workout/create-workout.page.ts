4e389c6f7f0783848e6256bacb8b785eee4dcd03
Luca Azmanov <72404660+Aztrixs@users.noreply.github.com>
2021-06-01 10:00:32 +0200

    Created file structure and instantiated new theme var for Create Workout Use Case - Luca Azmanov

---
+++ front-end/src/app/create-workout/create-workout.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-workout',
+  templateUrl: './create-workout.page.html',
+  styleUrls: ['./create-workout.page.scss'],
+})
+export class CreateWorkoutPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
