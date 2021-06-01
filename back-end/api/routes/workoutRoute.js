ac27a6a27083d43212231a1430474be10cf6465f
slickjumper <u19050412@tuks.co.za>
2021-06-01 09:35:21 +0200

    getworkoutbytitle swagger impl in workoutRoutes - Tinashe Chamisa

--- back-end/api/routes/workoutRoute.js
+++ back-end/api/routes/workoutRoute.js
@@ -11,11 +11,42 @@ const workoutController = require("../controllers/workoutController")
  *          summary: Returns a list of workouts
  *          description: Gets all the workouts in the database that have the same title provided
  *          parameters:
- *          - title: title of the workout
+ *              - name: title
+ *                in: path
+ *                required: true
+ *                description: The title of the workout
+ *                schema:
+ *                      type: string
+ *                example: Push/Pull
  *
  *          responses:
  *              200:
- *                  description: Success
+*                   description: A workout object.
+*                   content:
+*                       application/json:
+*                           schema:
+*                               type: object
+*                               properties:
+*                                   workoutTitle:
+*                                       type: string
+*                                       example: Push/Pull
+*                                   workoutDescription:
+*                                       type: string
+*                                       example: Exercises that focus on pushing the weight
+*                                   exercises:
+*                                       type: array
+*                                   difficulty:
+*                                      type: string
+*                                      example: EASY/MEDIUM/HARD/EXTREME
+*                                   planner_Email:
+*                                      type: string
+*                                      example: planner@email.com
+*              400:
+*                   description: The specified title is invalid.
+*              404:
+*                   description: A workout with the specified title was not found.
+*              default:
+*                   description: Unexpected error
  */
 router.get('/getworkoutbytitle/:title', workoutController.getWorkoutByTitle);
 router.get('/getexercisebytitle/:title', workoutController.getExerciseByTitle);
