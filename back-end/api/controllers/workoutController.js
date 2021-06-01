fc4c055838e3d1c184a749ef37da5ddf02b5b250
slickjumper <u19050412@tuks.co.za>
2021-06-01 10:23:33 +0200

    created validate email, validate json object and complete getWorkoutByPlanner in workoutController - Tinashe Chamisa

--- back-end/api/controllers/workoutController.js
+++ back-end/api/controllers/workoutController.js
@@ -4,6 +4,15 @@ const prisma = new PrismaClient()
 
 const bcrypt= require('bcrypt');
 
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function isEmptyObject(obj) {
+    return !Object.keys(obj).length;
+}
+
 exports.getWorkoutByTitle=async (req,res,next) =>
 {
     const workouts = await prisma.workout.findMany({
@@ -53,21 +62,35 @@ exports.getExerciseByTitle=async (req,res,next) =>
 
 exports.getWorkoutByPlanner=async (req,res,next) =>
 {
-    const workouts = await prisma.workout.findMany({
-        where: {
-            planner_Email : req.params.email
-        },
-        select: {
-            workoutTitle: true,
-            workoutDescription: true,
-            exercises: true,
-            difficulty: true,
-            planner_Email: true
-        }
-    });
+    if(validateEmail(req.params.email)==false){
+        res.status(400).json({
+            error:"Invalid email passed in."
+        });
+    }
+    else{
+        const workouts = await prisma.workout.findMany({
+            where: {
+                planner_Email : req.params.email
+            },
+            select: {
+                workoutTitle: true,
+                workoutDescription: true,
+                exercises: true,
+                difficulty: true,
+                planner_Email: true
+            }
+        });
 
-    res.status(200).json({
-        message: "Successfully retrieved workouts",
-        data: workouts
-    });
+        if(isEmptyObject(workouts)){
+            res.status(404).json({
+                message: "Unsuccessful. Planner does not exist in the database."
+            });
+        }
+        else{
+            res.status(200).json({
+                message: "Successfully retrieved workouts.",
+                data: workouts
+            });
+        }
+    }
 }
