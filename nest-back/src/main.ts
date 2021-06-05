88733796dd9ae5401023b5aec9f2729e22cd9410
slickjumper <u19050412@tuks.co.za>
2021-06-05 17:39:45 +0200

    added swagger to main.ts + install swagger nestjs - Tinashe Chamisa

--- nest-back/src/main.ts
+++ nest-back/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+      .setTitle('Kenzo Workout Api')
+      .setDescription('The get and post request for Kenzo Workout')
+      .setVersion('1.0')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
