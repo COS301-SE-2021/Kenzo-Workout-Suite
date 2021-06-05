12b956031ad49c0954b4123b6086b77c40718b94
slickjumper <u19050412@tuks.co.za>
2021-06-05 14:37:14 +0200

    added prisma service to workout.module.ts - Tinashe Chamisa

--- nest-back/src/Workout/workout.module.ts
+++ nest-back/src/Workout/workout.module.ts
@@ -1,11 +1,12 @@
 import {Module} from '@nestjs/common';
 import {WorkoutController} from "./workout.controller";
 import {WorkoutService} from "./workoutService";
+import {PrismaService} from "../Prisma/prisma.service";
 
 @Module(
     {
         controllers:[WorkoutController],
-        providers: [WorkoutService],
+        providers: [WorkoutService, PrismaService],
     }
 )
 
