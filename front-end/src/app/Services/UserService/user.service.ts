d7da94fa659b9346e24cf7f83f68acb7810e6d3b
Luca Azmanov <72404660+Aztrixs@users.noreply.github.com>
2021-06-09 23:35:47 +0200

    Added UserService with SignIn implementation - Luca Azmanov

---
+++ front-end/src/app/Services/UserService/user.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {AlertController} from "@ionic/angular";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http:HttpClient) { }
+
+  /**
+   *
+   * @param email
+   * @param password
+   */
+  async attemptSignIn(email: string, password: string): Promise<Number> {
+    const url: string = "http://localhost:5500/user/signIn";
+
+    //Client Validation
+    if (email == null) {
+      email = "";
+    }
+
+    if (password == null) {
+      password = "";
+    }
+    const body: Object = {
+      "email": email,
+      "password": password
+    };
+
+    return this.http.post(url, body).toPromise().then(r => {
+          return 200;
+        }).catch((error)=>{
+        return error.status;
+      });
+  }
+}
