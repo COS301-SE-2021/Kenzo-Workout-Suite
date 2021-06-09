f700aa0abf13b3bd010d4b4770fd1b118ec9534d
Zeee2000 <u19086688@tuks.co.za>
2021-06-10 00:25:17 +0200

    Fully implemented signUp and Login with security measures in place - Zelealem Tesema

--- nest-back/src/user/local.strategy.ts
+++ nest-back/src/user/local.strategy.ts
@@ -9,11 +9,13 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
         super();
     }
 
-    async validate(username: string, password: string): Promise<any> {
-        const user = await this.userService.validateUser(username, password);
+    async validate(email: string, password: string): Promise<any> {
+        const user = await this.userService.validateUser(email, password);
         if (!user) {
             throw new UnauthorizedException();
         }
         return user;
     }
+
+
 }
--- nest-back/src/user/user.controller.ts
+++ nest-back/src/user/user.controller.ts
@@ -1,14 +1,10 @@
 import {Body, Controller, Get, Post, Put, UseGuards,Request} from '@nestjs/common';
 import {UserService} from "./user.service";
-import {
-    Workout,
-    Exercise,
-    Planner,
-    Difficulty,
-    Prisma
-} from '@prisma/client';
-import {Client} from "@prisma/client";
 import { AuthGuard } from '@nestjs/passport';
+import {LocalAuthGuard} from "./local-auth.guard";
+import {JwtAuthGuard} from "./jwt-auth.guard";
+import {Client} from "@prisma/client";
+
 
 @Controller('user')
 export class UserController {
@@ -16,9 +12,23 @@ export class UserController {
     constructor(private readonly userService: UserService) {
     }
 
-    @UseGuards(AuthGuard('local'))
+    @UseGuards(LocalAuthGuard)
     @Post('login')
     async login(@Request() req) {
+        return this.userService.login(req.user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('protected')
+    doSomething(@Request() req){
         return req.user;
     }
+
+    @Post('signUpClient')
+    signUpClient(
+        @Body('Client') client: Client,
+    ) {
+        return this.userService.signUpClient(client);
+    }
+
 }
