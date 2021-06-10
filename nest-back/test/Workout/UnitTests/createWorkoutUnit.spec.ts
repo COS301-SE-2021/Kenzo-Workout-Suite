d05484077cda6c898cade73f4d069b57159f7fe8
Msi <msi.game.sibs@gmail.com>
2021-06-11 00:51:29 +0200

    CreateWorkout Unit Tests [surely not all] -Msi

--- nest-back/test/Workout/UnitTests/createWorkoutUnit.spec.ts
+++ nest-back/test/Workout/UnitTests/createWorkoutUnit.spec.ts
@@ -20,7 +20,7 @@ beforeEach(() => {
     ctx = (mockCtx as unknown) as Context
 })
 
-test('should create new workout ', async () => {
+test('Should create new workout ', async () => {
     const Workout = {
         workoutID: uuidv4(),
         workoutTitle: "postmanTest",
@@ -32,5 +32,50 @@ test('should create new workout ', async () => {
 
     await expect(workoutService.createWorkout(Workout.workoutTitle,Workout.workoutDescription, Workout.difficulty, Workout.planner_Email,ctx)).resolves.toEqual(
         "Workout Created."
+    )
+})
+
+test('Should not create workout [Throws Empty Parameters error(Title)] ', async () => {
+    const Workout = {
+        workoutID: uuidv4(),
+        workoutTitle: "",
+        workoutDescription: "postmanMoreTest",
+        difficulty: Difficulty.EASY,
+        planner_Email: "null"
+    }
+    mockCtx.prisma.workout.create.mockResolvedValue(Workout);
+
+    await expect(workoutService.createWorkout(Workout.workoutTitle,Workout.workoutDescription, Workout.difficulty, Workout.planner_Email,ctx)).rejects.toThrow(
+        "Parameters can not be left empty."
+    )
+})
+
+test('Should not create workout [Throws Empty Parameters error(Description)] ', async () => {
+    const Workout = {
+        workoutID: uuidv4(),
+        workoutTitle: "postmanMoreTest",
+        workoutDescription: "",
+        difficulty: Difficulty.EASY,
+        planner_Email: "null"
+    }
+    mockCtx.prisma.workout.create.mockResolvedValue(Workout);
+
+    await expect(workoutService.createWorkout(Workout.workoutTitle,Workout.workoutDescription, Workout.difficulty, Workout.planner_Email,ctx)).rejects.toThrow(
+        "Parameters can not be left empty."
+    )
+})
+
+test('Should not create workout [Throws Empty Parameters error(Both String Inputs)] ', async () => {
+    const Workout = {
+        workoutID: uuidv4(),
+        workoutTitle: "",
+        workoutDescription: "",
+        difficulty: Difficulty.HARD,
+        planner_Email: "null"
+    }
+    mockCtx.prisma.workout.create.mockResolvedValue(Workout);
+
+    await expect(workoutService.createWorkout(Workout.workoutTitle,Workout.workoutDescription, Workout.difficulty, Workout.planner_Email,ctx)).rejects.toThrow(
+        "Parameters can not be left empty."
     )
 })
