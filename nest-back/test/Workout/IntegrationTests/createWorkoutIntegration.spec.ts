88864af9b5c5fddb45e557daaef7c00efcc0b9bb
Msi <msi.game.sibs@gmail.com>
2021-06-10 10:39:08 +0200

    Fixing of file structure for Unit and Integration Tests with example of UnitTest -Msi

---
+++ nest-back/test/Workout/IntegrationTests/createWorkoutIntegration.spec.ts
@@ -0,0 +1,36 @@
+import { MockContext, Context, createMockContext } from "../../../context";
+import {WorkoutService} from "../../../src/Workout/workout.service";
+import { Test, TestingModule } from '@nestjs/testing';
+import {v4 as uuidv4 } from 'uuid';
+import {
+    Workout,
+    Exercise,
+    Planner,
+    Difficulty,
+    Prisma
+} from '@prisma/client';
+
+let mockCtx: MockContext
+let ctx: Context
+let workoutService: WorkoutService
+
+beforeEach(() => {
+    workoutService = new WorkoutService;
+    mockCtx = createMockContext()
+    ctx = (mockCtx as unknown) as Context
+})
+
+test('should create new workout ', async () => {
+    const Workout = {
+        workoutID: uuidv4(),
+        workoutTitle: "postmanTest",
+        workoutDescription: "postmanMoreTest",
+        difficulty: Difficulty.EASY,
+        planner_Email: null
+    }
+    mockCtx.prisma.workout.create.mockResolvedValue(Workout);
+
+    await expect(workoutService.createWorkout(Workout.workoutTitle,Workout.workoutDescription, Workout.difficulty,ctx)).resolves.toEqual(
+        "Workout Created."
+    )
+})
---
+++ nest-back/test/Workout/UnitTests/createExerciseUnit.spec.ts
@@ -0,0 +1,44 @@
+import { MockContext, Context, createMockContext } from "../../../context";
+import {WorkoutService} from "../../../src/Workout/workout.service";
+//import { Test, TestingModule } from '@nestjs/testing';
+import {v4 as uuidv4 } from 'uuid';
+import {
+    Workout,
+    Exercise,
+    Planner,
+    Difficulty,
+    Prisma
+} from '@prisma/client';
+
+let mockCtx: MockContext
+let ctx: Context
+let workoutService: WorkoutService
+
+beforeEach(() => {
+    workoutService = new WorkoutService;
+    mockCtx = createMockContext()
+    ctx = (mockCtx as unknown) as Context
+})
+
+
+
+test('should create new exercise ', async () => {
+    const Exercise = {
+        exercise:uuidv4(),
+        title:"TestExercise",
+        description:"TestDescription",
+        repRange:"TestRange",
+        sets:4,
+        Posedescription:"TestPDesc",
+        restPeriod:2,
+        difficulty:Difficulty.EASY,
+
+        duratime:2,
+        workoutWorkoutID: null
+    }
+    mockCtx.prisma.exercise.create.mockResolvedValue(Exercise)
+
+    await expect(workoutService.createExercise(Exercise.title,Exercise.description,Exercise.repRange,Exercise.sets,Exercise.Posedescription,Exercise.restPeriod,Exercise.difficulty,Exercise.duratime,ctx)).resolves.toEqual(
+        "Exercise created."
+    )
+})
