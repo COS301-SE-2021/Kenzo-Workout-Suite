e898f6f2482512d0d0f934f1ee3f53e8ced40683
slickjumper <u19050412@tuks.co.za>
2021-06-12 12:49:19 +0200

    complete getWorkoutsByTitle Unit test - Tinashe Chamisa

--- nest-back/test/Workout/UnitTests/getWorkoutByTitle.spec.ts
+++ nest-back/test/Workout/UnitTests/getWorkoutByTitle.spec.ts
@@ -5,23 +5,36 @@ import {v4 as uuidv4 } from 'uuid';
 import {
     Workout,
     Exercise,
-    Prisma
+    Prisma, Difficulty
 } from '@prisma/client';
+import {PrismaClient} from "@prisma/client/scripts/default-index";
 
 let mockCtx: MockContext
 let ctx: Context
 let workoutService: WorkoutService
+let prisma: PrismaClient
 
 beforeEach(() => {
-    workoutService = new WorkoutService;
+    workoutService = new WorkoutService(prisma);
     mockCtx = createMockContext()
     ctx = (mockCtx as unknown) as Context
 })
 
 test('Should recieve valid information about workout with corresponding title', async () => {
-    //mockCtx.prisma.workout.findMany.mockResolvedValue()
+    const workout = [{
+        workoutID: uuidv4(),
+        workoutTitle: "test",
+        workoutDescription: "test",
+        difficulty: Difficulty.EASY,
+        planner_Email: "test@gmail.com"
+    }]
+    mockCtx.prisma.workout.findMany.mockResolvedValue(workout)
+
+    const response=await workoutService.getWorkoutByTitle("test")
+
+    expect(response).toBe(workout);
 })
 
 test('Should not recieve valid information about workout with corresponding title as workout does not exist', async () => {
-    //mockCtx.prisma.workout.findMany.mockResolvedValue()
+    await expect(workoutService.getWorkoutByTitle("")).rejects.toThrow("No workouts were found in the database with the specified title.")
 })
