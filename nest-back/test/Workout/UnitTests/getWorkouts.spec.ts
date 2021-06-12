4229dfa5859646ccefc04d2ef721365a9eb328ae
slickjumper <u19050412@tuks.co.za>
2021-06-12 12:49:08 +0200

    complete getWorkouts Unit test - Tinashe Chamisa

--- nest-back/test/Workout/UnitTests/getWorkouts.spec.ts
+++ nest-back/test/Workout/UnitTests/getWorkouts.spec.ts
@@ -9,17 +9,30 @@ import {
     Difficulty,
     Prisma
 } from '@prisma/client';
+import {PrismaClient} from "@prisma/client/scripts/default-index";
 
 let mockCtx: MockContext
 let ctx: Context
 let workoutService: WorkoutService
+let prisma: PrismaClient
 
 beforeEach(() => {
-    workoutService = new WorkoutService;
+    workoutService = new WorkoutService(prisma);
     mockCtx = createMockContext()
     ctx = (mockCtx as unknown) as Context
 })
 
 test('Should receive valid information about all workouts', async () => {
-    //mockCtx.prisma.workout.findMany.mockResolvedValue()
+    const workout = [{
+        workoutID: uuidv4(),
+        workoutTitle: "test",
+        workoutDescription: "test",
+        difficulty: Difficulty.EASY,
+        planner_Email: "test@gmail.com"
+    }]
+    mockCtx.prisma.workout.findMany.mockResolvedValue(workout)
+
+    const response=await workoutService.getWorkouts()
+
+    expect(response).toBe(workout);
 })
