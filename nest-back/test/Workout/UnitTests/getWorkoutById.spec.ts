7aa8e452fe3c6d36e98e939f775265e94eaa72ca
slickjumper <u19050412@tuks.co.za>
2021-06-17 22:12:10 +0200

    changed getWorkoutByTitle.spec.ts to getWorkoutById.spec.ts in unit and integration testing - Tinashe Chamisa

--- nest-back/test/Workout/UnitTests/getWorkoutById.spec.ts
+++ nest-back/test/Workout/UnitTests/getWorkoutById.spec.ts
@@ -13,6 +13,7 @@ let mockCtx: MockContext
 let ctx: Context
 let workoutService: WorkoutService
 let prisma: PrismaClient
+const uuidWorkout = uuidv4();
 
 beforeEach(() => {
     workoutService = new WorkoutService(prisma);
@@ -20,21 +21,21 @@ beforeEach(() => {
     ctx = (mockCtx as unknown) as Context
 })
 
-test('Should receive valid information about workout with corresponding title', async () => {
-    const workout = [{
-        workoutID: uuidv4(),
+test('Should receive valid information about workout with corresponding id', async () => {
+    const workout = {
+        workoutID: uuidWorkout,
         workoutTitle: "test",
         workoutDescription: "test",
         difficulty: Difficulty.EASY,
         planner_ID: uuidv4()
-    }]
-    mockCtx.prisma.workout.findMany.mockResolvedValue(workout)
+    }
+    mockCtx.prisma.workout.findUnique.mockResolvedValue(workout)
 
-    const response=await workoutService.getWorkoutByTitle("test",ctx)
+    const response=await workoutService.getWorkoutById(uuidWorkout,ctx)
 
     expect(response).toBe(workout);
 })
 
-test('Should not receive valid information about workout with corresponding title as workout does not exist', async () => {
-    await expect(workoutService.getWorkoutByTitle("",ctx)).rejects.toThrow("No workouts were found in the database with the specified title.")
+test('Should not receive valid information about workout with corresponding id as workout does not exist', async () => {
+    await expect(workoutService.getWorkoutById("",ctx)).rejects.toThrow("No workouts were found in the database with the specified id.")
 })
