10ab59d3cd7f62b19607ef55542a9377ef142fda
Zeee2000 <u19086688@tuks.co.za>
2021-06-17 19:44:06 +0200

    Implemented User endpoint testing  - Zelealem Tesema

--- nest-back/src/main.ts
+++ nest-back/src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
      .setTitle('Kenzo-Workout Suite')
      .setDescription('Description of Kenzo-Workout Suite API')
      .setVersion('1.0')
+      .addBearerAuth()
      .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
