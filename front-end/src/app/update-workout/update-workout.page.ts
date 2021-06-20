f88b856114811588ecdece44359506c574122295
Luca Azmanov <72404660+Aztrixs@users.noreply.github.com>
2021-06-20 20:54:20 +0200

    Delete Working - Luca Azmanov

--- front-end/src/app/update-workout/update-workout.page.ts
+++ front-end/src/app/update-workout/update-workout.page.ts
@@ -121,6 +121,72 @@ export class UpdateWorkoutPage implements OnInit {
     }
   }
 
+  /** This function uses the workout service to submit a request to delete a workout.
+   * The workout service will return the status of the request:
+   * 200 -> Success
+   * 400 -> Incorrect Data
+   * 500 -> Server not responding
+   *
+   * Thereafter,
+   * Error states [400,500] will result in an alert
+   * Success states [200] will result in a logged in a Planner being navigated to the logged in User's homescreen.
+   * @author Luca Azmanov, u19004185
+   */
+  async submitDeleteRequest() {
+    let confirmation = false;
+    const alert = await this.alertController.create({
+      cssClass: 'kenzo-alert',
+      header: 'Are you sure you would like to delete this workout?',
+      buttons: [{text:'Delete',
+        handler: (confirm)=>{
+          confirmation = true;
+        }},'Cancel']
+    });
+
+    await this.presentAlert(alert);
+    if(!confirmation) return;
+
+    let status = await this.workoutService.attemptRemoveWorkout(this.id);
+
+    if (status < 400) {
+      // Success State
+      const alert = await this.alertController.create({
+        cssClass: 'kenzo-alert',
+        header: 'Workout Deleted',
+        buttons: ['Go Back']
+      });
+
+      await this.presentAlert(alert);
+      this.route.navigate(['/your-workouts']).then(success=>{
+        this.reloadWindow();}
+      );
+      return 200;
+    }
+    else if(status>=400 && status<500){
+      // Invalid Input
+      const alert = await this.alertController.create({
+        cssClass: 'kenzo-alert',
+        header: 'Could not delete workout',
+        message: 'Please try again later.',
+        buttons: ['Dismiss']
+      });
+
+      await this.presentAlert(alert);
+      throw new Error("Data is invalid.");
+    }
+    else{
+      // Server Error
+      const alert = await this.alertController.create({
+        cssClass: 'kenzo-alert',
+        header: "Server isn't responding",
+        message: 'Please try again later.',
+        buttons: ['Dismiss']
+      });
+      await this.presentAlert(alert);
+      throw new Error("Server is not responding.");
+    }
+  }
+
   /**
    * Helper Function to physically present alert to user independent of OS.
    * @param alert
