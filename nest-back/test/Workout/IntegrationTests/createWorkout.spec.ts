0139404c53f98de56654aa8648a31c947fb08d19
Msi <msi.game.sibs@gmail.com>
2021-07-06 03:06:46 +0200

    ESLinted Integration test files and fixed all my Integration Tests - Msi

--- nest-back/test/Workout/IntegrationTests/createWorkout.spec.ts
+++ nest-back/test/Workout/IntegrationTests/createWorkout.spec.ts
@@ -1,175 +1,116 @@
-import {MockContext, Context, createMockContext, ActualPrisma} from "../../../context";
-import {WorkoutService} from "../../../src/Workout/workout.service";
-import {v4 as uuidv4 } from 'uuid';
+import { Context, ActualPrisma } from "../../../context"
+import { WorkoutService } from "../../../src/Workout/workout.service"
+import { v4 as uuidv4 } from "uuid"
 import {
-    Workout,
-    Exercise,
-    User,
-    Tag,
-    Prisma, userType
-} from '@prisma/client';
-import {PrismaClient} from "@prisma/client/scripts/default-index";
+  Exercise,
+  userType
+} from "@prisma/client"
+import { PrismaClient } from "@prisma/client/scripts/default-index"
 
-let mockCtx: MockContext
 let ctx: Context
 let workoutService: WorkoutService
 let prisma: PrismaClient
-let userUUID=uuidv4();
-let exerciseUUID=uuidv4();
+const userUUID = uuidv4()
+const exerciseUUID = uuidv4()
 
-describe('Integration test for createWorkout for the User subsystem', () => {
+describe("Integration test for createWorkout for the Workout Service", () => {
+  beforeEach(async () => {
+    workoutService = new WorkoutService(prisma)
+    ctx = ActualPrisma()
+    await ctx.prisma.exercise.deleteMany()
+    await ctx.prisma.user.deleteMany()
+    await ctx.prisma.tag.deleteMany()
+    const myUser = {
+      userID: userUUID,
+      email: "test@gmail.com",
+      firstName: "test",
+      lastName: "tester",
+      password: "Test123*",
+      userType: userType.PLANNER,
+      dateOfBirth: null
+    }
 
-    beforeEach(async () => {
-        workoutService = new WorkoutService(prisma);
-        mockCtx = createMockContext();
-        ctx = ActualPrisma();
-        await ctx.prisma.exercise.deleteMany();
-        await ctx.prisma.user.deleteMany();
-        await ctx.prisma.tag.deleteMany();
-        const myUser = {
-            userId: userUUID,
-            email: "test@gmail.com",
-            firstName: "test",
-            lastName: "tester",
-            password: "Test123*",
-            userType: userType.PLANNER,
-            dateOfBirth: null
-        }
+    await ctx.prisma.user.create({
+      data: myUser
+    })
+  })
 
-        await ctx.prisma.user.create({
-            "data": myUser
-        })
+  test("Should create workout [no exercises]", async () => {
+    const workoutUUID = uuidv4()
+    const Workout = {
+      workoutID: workoutUUID,
+      workoutTitle: "Test",
+      workoutDescription: "Test",
+      planner_ID: userUUID
+    }
 
+    const emptyExercise: Exercise[] = []
+
+    await expect(workoutService.createWorkout(Workout.workoutTitle, Workout.workoutDescription, emptyExercise, Workout.planner_ID, ctx)).resolves.toEqual(
+      "Workout Created."
+    )
+  })
+
+  test("Should create new workout [With exercises]", async () => {
+    const workoutUUID = uuidv4()
+    const Workout = {
+      workoutID: workoutUUID,
+      workoutTitle: "Test",
+      workoutDescription: "Test",
+      planner_ID: userUUID
+    }
+
+    const Exercise = {
+      exerciseID: exerciseUUID,
+      exerciseTitle: "TestExercise",
+      exerciseDescription: "TestDescription",
+      repRange: "TestRange",
+      sets: 4,
+      poseDescription: "TestPDesc",
+      restPeriod: 2,
+      duration: 2,
+      plannerID: userUUID
+    }
+    await ctx.prisma.exercise.create({
+      data: Exercise
     })
 
-    test('Should create workout [no tags or exercises]', async () => {
+    const fullExercise = [Exercise]
 
-        let workoutUUID = uuidv4();
-        const Workout = {
-            workoutID: workoutUUID,
-            workoutTitle: "Test",
-            workoutDescription: "Test",
-            planner_ID: userUUID
-        }
-        let emptyTag: Tag[] = [];
-        let emptyExercise: Exercise[] = [];
+    await expect(workoutService.createWorkout(Workout.workoutTitle, Workout.workoutDescription, fullExercise, Workout.planner_ID, ctx)).resolves.toEqual(
+      "Workout Created."
+    )
+  })
 
-        await expect(workoutService.createWorkout(Workout.workoutTitle, Workout.workoutDescription, emptyExercise, emptyTag, Workout.planner_ID, ctx)).resolves.toEqual(
-            "Workout Created."
-        )
-    })
+  test("Should not create workout [Missing Title - Throws Parameters can not be left empty. ]", async () => {
+    const workoutUUID = uuidv4()
+    const Workout = {
+      workoutID: workoutUUID,
+      workoutTitle: "",
+      workoutDescription: "Test",
+      planner_ID: userUUID
+    }
 
-    test('Should create new workout [With tags and exercises]', async () => {
+    const emptyExercise: Exercise[] = []
 
-        let workoutUUID = uuidv4();
-        const Workout = {
-            workoutID: workoutUUID,
-            workoutTitle: "Test",
-            workoutDescription: "Test",
-            planner_ID: userUUID
-        }
-        let tagArray: Tag[] = [{"label": "painful", "textColour": "blue", "backgroundColour": "white"}];
-        const Exercise = {
-            exercise: exerciseUUID,
-            title: "TestExercise",
-            description: "TestDescription",
-            repRange: "TestRange",
-            sets: 4,
-            Posedescription: "TestPDesc",
-            restPeriod: 2,
-            duratime: 2,
-            planner_ID: userUUID
-        }
-        await ctx.prisma.exercise.create({
-            "data": Exercise
-        })
-        let emptyExercise: Exercise[] = [];
-        let fullExercise = [Exercise];
+    await expect(workoutService.createWorkout(Workout.workoutTitle, Workout.workoutDescription, emptyExercise, Workout.planner_ID, ctx)).rejects.toThrow(
+      "Parameters can not be left empty."
+    )
+  })
 
-        await expect(workoutService.createWorkout(Workout.workoutTitle, Workout.workoutDescription, fullExercise, tagArray, Workout.planner_ID, ctx)).resolves.toEqual(
-            "Workout Created."
-        )
-    })
+  test("Should not create workout [Missing Description - Throws Parameters can not be left empty.]", async () => {
+    const workoutUUID = uuidv4()
+    const Workout = {
+      workoutID: workoutUUID,
+      workoutTitle: "Test",
+      workoutDescription: "",
+      planner_ID: userUUID
+    }
 
-    test('Should create new workout [With tags only]', async () => {
+    const emptyExercise: Exercise[] = []
 
-        let workoutUUID = uuidv4();
-        const Workout = {
-            workoutID: workoutUUID,
-            workoutTitle: "Test",
-            workoutDescription: "Test",
-            planner_ID: userUUID
-        }
-        let tagArray: Tag[] = [{"label": "painful", "textColour": "blue", "backgroundColour": "white"}];
-        let emptyExercise: Exercise[] = [];
-
-        await expect(workoutService.createWorkout(Workout.workoutTitle, Workout.workoutDescription, emptyExercise, tagArray, Workout.planner_ID, ctx)).resolves.toEqual(
-            "Workout Created."
-        )
-    })
-
-    test('Should create new workout [With exercises only]', async () => {
-
-        let workoutUUID = uuidv4();
-        const Workout = {
-            workoutID: workoutUUID,
-            workoutTitle: "Test",
-            workoutDescription: "Test",
-            planner_ID: userUUID
-        }
-        let emptyTags: Tag[] = [];
-        const Exercise = {
-            exercise: exerciseUUID,
-            title: "TestExercise",
-            description: "TestDescription",
-            repRange: "TestRange",
-            sets: 4,
-            Posedescription: "TestPDesc",
-            restPeriod: 2,
-            duratime: 2,
-            planner_ID: userUUID
-        }
-        await ctx.prisma.exercise.create({
-            "data": Exercise
-        })
-        let fullExercise = [Exercise];
-
-        await expect(workoutService.createWorkout(Workout.workoutTitle, Workout.workoutDescription, fullExercise, emptyTags, Workout.planner_ID, ctx)).resolves.toEqual(
-            "Workout Created."
-        )
-    })
-
-    test('Should not create workout [Missing Title - Throws Parameters can not be left empty. ]', async () => {
-
-        let workoutUUID = uuidv4();
-        const Workout = {
-            workoutID: workoutUUID,
-            workoutTitle: "",
-            workoutDescription: "Test",
-            planner_ID: userUUID
-        }
-        let emptyTag: Tag[] = [];
-        let emptyExercise: Exercise[] = [];
-
-        await expect(workoutService.createWorkout(Workout.workoutTitle, Workout.workoutDescription, emptyExercise, emptyTag, Workout.planner_ID, ctx)).rejects.toThrow(
-            "Parameters can not be left empty."
-        )
-    })
-
-    test('Should not create workout [Missing Description - Throws Parameters can not be left empty.]', async () => {
-
-        let workoutUUID = uuidv4();
-        const Workout = {
-            workoutID: workoutUUID,
-            workoutTitle: "Test",
-            workoutDescription: "",
-            planner_ID: userUUID
-        }
-        let emptyTag: Tag[] = [];
-        let emptyExercise: Exercise[] = [];
-
-        await expect(workoutService.createWorkout(Workout.workoutTitle, Workout.workoutDescription, emptyExercise, emptyTag, Workout.planner_ID, ctx)).rejects.toThrow(
-            "Parameters can not be left empty."
-        )
-    })
+    await expect(workoutService.createWorkout(Workout.workoutTitle, Workout.workoutDescription, emptyExercise, Workout.planner_ID, ctx)).rejects.toThrow(
+      "Parameters can not be left empty."
+    )
+  })
 })
--- nest-back/test/Workout/IntegrationTests/updateWorkout.spec.ts
+++ nest-back/test/Workout/IntegrationTests/updateWorkout.spec.ts
@@ -1,203 +1,143 @@
-import {MockContext, Context, createMockContext, ActualPrisma} from "../../../context";
-import {WorkoutService} from "../../../src/Workout/workout.service";
-import {v4 as uuidv4 } from 'uuid';
+import { Context, ActualPrisma } from "../../../context"
+import { WorkoutService } from "../../../src/Workout/workout.service"
+import { v4 as uuidv4 } from "uuid"
 import {
-    Workout,
-    Exercise,
-    User,
-    Tag,
-    Prisma, userType
-} from '@prisma/client';
-import {PrismaClient} from "@prisma/client/scripts/default-index";
+  Exercise,
+  userType
+} from "@prisma/client"
+import { PrismaClient } from "@prisma/client/scripts/default-index"
 
-let mockCtx: MockContext
 let ctx: Context
 let workoutService: WorkoutService
 let prisma: PrismaClient
-let userUUID=uuidv4();
-let exerciseUUID=uuidv4();
+const userUUID = uuidv4()
+const exerciseUUID = uuidv4()
 
-const Exercise = {
-    exercise:exerciseUUID,
-    title:"TestExercise",
-    description:"TestDescription",
-    repRange:"TestRange",
-    sets:4,
-    Posedescription:"TestPDesc",
-    restPeriod:2,
-    duratime:2,
-    planner_ID:userUUID
+const exercise = {
+  exerciseID: exerciseUUID,
+  exerciseTitle: "TestExercise",
+  exerciseDescription: "TestDescription",
+  repRange: "TestRange",
+  sets: 4,
+  poseDescription: "TestPDesc",
+  restPeriod: 2,
+  duration: 2,
+  plannerID: userUUID
 }
 
-
 beforeEach(async () => {
-    workoutService = new WorkoutService(prisma);
-    mockCtx = createMockContext();
-    ctx = ActualPrisma();
-    await ctx.prisma.exercise.deleteMany();
-    await ctx.prisma.user.deleteMany();
-    await ctx.prisma.tag.deleteMany();
-    const myUser={
-        userId:userUUID,
-        email: "test@gmail.com",
-        firstName: "test",
-        lastName: "tester",
-        password:"Test123*",
-        userType: userType.PLANNER,
-        dateOfBirth: null
-    }
+  workoutService = new WorkoutService(prisma)
+  ctx = ActualPrisma()
+  await ctx.prisma.exercise.deleteMany()
+  await ctx.prisma.user.deleteMany()
+  await ctx.prisma.tag.deleteMany()
+  const myUser = {
+    userID: userUUID,
+    email: "test@gmail.com",
+    firstName: "test",
+    lastName: "tester",
+    password: "Test123*",
+    userType: userType.PLANNER,
+    dateOfBirth: null
+  }
 
-    await ctx.prisma.user.create({
-        "data":myUser
-    })
+  await ctx.prisma.user.create({
+    data: myUser
+  })
 
-    await ctx.prisma.exercise.create({
-        "data":Exercise
-    })
+  await ctx.prisma.exercise.create({
+    data: exercise
+  })
 })
-
-test('Should update workout [no tags or exercises]', async () => {
-
-    let workoutUUID =uuidv4();
+describe("Integration tests of the updateWorkout function in the Workout Service", () => {
+  test("Should update workout [no exercises]", async () => {
+    const workoutUUID = uuidv4()
     const Workout = {
-        workoutID: workoutUUID,
-        workoutTitle: "Test",
-        workoutDescription: "Test",
-        planner: {
-            connect: {
-                userId: userUUID
-            }
+      workoutID: workoutUUID,
+      workoutTitle: "Test",
+      workoutDescription: "Test",
+      planner: {
+        connect: {
+          userID: userUUID
         }
+      }
     }
     await ctx.prisma.workout.create({
-        data: Workout
+      data: Workout
     })
-    let emptyTag: Tag[] = [];
-    let emptyExercise: Exercise[] = [];
 
-    await expect(workoutService.updateWorkout(workoutUUID,"WorkoutUpdateTest",Workout.workoutDescription,emptyExercise,emptyTag,userUUID,ctx)).resolves.toEqual(
-        "Workout Updated."
+    const emptyExercise: Exercise[] = []
+
+    await expect(workoutService.updateWorkout(workoutUUID, "WorkoutUpdateTest", Workout.workoutDescription, emptyExercise, userUUID, ctx)).resolves.toEqual(
+      "Workout Updated."
     )
-})
+  })
 
-test('Should update new workout [With tags and exercises]', async () => {
-
-
-    let tagArray: Tag[] = [{"label":"painful","textColour":"blue","backgroundColour":"white"}];
-
-    let workoutUUID =uuidv4();
+  test("Should update new workout [With exercises]", async () => {
+    const workoutUUID = uuidv4()
     const Workout = {
-        workoutID: workoutUUID,
-        workoutTitle: "Test",
-        workoutDescription: "Test",
-        planner: {
-            connect: {
-                userId: userUUID
-            }
+      workoutID: workoutUUID,
+      workoutTitle: "Test",
+      workoutDescription: "Test",
+      planner: {
+        connect: {
+          userID: userUUID
         }
+      }
     }
     await ctx.prisma.workout.create({
-        data: Workout
+      data: Workout
     })
-    let fullExercise = [Exercise];
+    const fullExercise = [exercise]
 
-    await expect(workoutService.updateWorkout(workoutUUID,"WorkoutUpdateTest",Workout.workoutDescription, fullExercise ,tagArray, userUUID,ctx)).resolves.toEqual(
-        "Workout Updated."
+    await expect(workoutService.updateWorkout(workoutUUID, "WorkoutUpdateTest", Workout.workoutDescription, fullExercise, userUUID, ctx)).resolves.toEqual(
+      "Workout Updated."
     )
-})
+  })
 
-test('Should update new workout [With tags only]', async () => {
-
-    let workoutUUID =uuidv4();
+  test("Should not update workout [Missing Title - Throws Parameters can not be left empty.]", async () => {
+    const workoutUUID = uuidv4()
     const Workout = {
-        workoutID: workoutUUID,
-        workoutTitle: "Test",
-        workoutDescription: "Test",
-        planner: {
-            connect: {
-                userId: userUUID
-            }
+      workoutID: workoutUUID,
+      workoutTitle: "Test",
+      workoutDescription: "Test",
+      planner: {
+        connect: {
+          userID: userUUID
         }
+      }
     }
     await ctx.prisma.workout.create({
-        data: Workout
+      data: Workout
     })
-    let tagArray: Tag[] = [{"label":"painful","textColour":"blue","backgroundColour":"white"}];
-    let emptyExercise: Exercise[] = [];
 
-    await expect(workoutService.updateWorkout(workoutUUID,"WorkoutUpdateTest",Workout.workoutDescription, emptyExercise ,tagArray, userUUID,ctx)).resolves.toEqual(
-        "Workout Updated."
+    const emptyExercise: Exercise[] = []
+
+    await expect(workoutService.updateWorkout(workoutUUID, "", Workout.workoutDescription, emptyExercise, userUUID, ctx)).rejects.toThrow(
+      "Parameters can not be left empty."
     )
-})
+  })
 
-test('Should update new workout [With exercises only]', async () => {
-
-    let workoutUUID =uuidv4();
+  test("Should not update workout [Missing Description - Throws Parameters can not be left empty.]", async () => {
+    const workoutUUID = uuidv4()
     const Workout = {
-        workoutID: workoutUUID,
-        workoutTitle: "Test",
-        workoutDescription: "Test",
-        planner: {
-            connect: {
-                userId: userUUID
-            }
+      workoutID: workoutUUID,
+      workoutTitle: "Test",
+      workoutDescription: "Test",
+      planner: {
+        connect: {
+          userID: userUUID
         }
+      }
     }
     await ctx.prisma.workout.create({
-        data: Workout
+      data: Workout
     })
-    let emptyTags: Tag[] = [];
-    let fullExercise = [Exercise];
 
-    await expect(workoutService.updateWorkout(workoutUUID,"WorkoutUpdateTest",Workout.workoutDescription, fullExercise ,emptyTags, userUUID,ctx)).resolves.toEqual(
-        "Workout Updated."
+    const emptyExercise: Exercise[] = []
+
+    await expect(workoutService.updateWorkout(workoutUUID, "WorkoutUpdateTest", "", emptyExercise, userUUID, ctx)).rejects.toThrow(
+      "Parameters can not be left empty."
     )
-})
-
-test('Should not update workout [Missing Title - Throws Parameters can not be left empty.]', async () => {
-
-    let workoutUUID =uuidv4();
-    const Workout = {
-        workoutID: workoutUUID,
-        workoutTitle: "Test",
-        workoutDescription: "Test",
-        planner: {
-            connect: {
-                userId: userUUID
-            }
-        }
-    }
-    await ctx.prisma.workout.create({
-        data: Workout
-    })
-    let emptyTag: Tag[] = [];
-    let emptyExercise: Exercise[] = [];
-
-    await expect(workoutService.updateWorkout(workoutUUID,"",Workout.workoutDescription,emptyExercise,emptyTag,userUUID,ctx)).rejects.toThrow(
-        "Parameters can not be left empty."
-    )
-})
-
-test('Should not update workout [Missing Description - Throws Parameters can not be left empty.]', async () => {
-
-    let workoutUUID =uuidv4();
-    const Workout = {
-        workoutID: workoutUUID,
-        workoutTitle: "Test",
-        workoutDescription: "Test",
-        planner: {
-            connect: {
-                userId: userUUID
-            }
-        }
-    }
-    await ctx.prisma.workout.create({
-        data: Workout
-    })
-    let emptyTag: Tag[] = [];
-    let emptyExercise: Exercise[] = [];
-
-    await expect(workoutService.updateWorkout(workoutUUID,"WorkoutUpdateTest","",emptyExercise,emptyTag,userUUID,ctx)).rejects.toThrow(
-        "Parameters can not be left empty."
-    )
+  })
 })
