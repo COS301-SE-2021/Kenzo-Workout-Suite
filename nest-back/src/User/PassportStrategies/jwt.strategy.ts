e26a58e482d3b86f07165ab03bdb082efd882017
Zeee2000 <u19086688@tuks.co.za>
2021-07-06 16:34:02 +0200

    Edited all files and ensured valid naming convention - Zelealem Tesema

--- nest-back/src/User/PassportStrategies/jwt.strategy.ts
+++ nest-back/src/User/PassportStrategies/jwt.strategy.ts
@@ -1,20 +1,20 @@
-import {PassportStrategy} from "@nestjs/passport";
-import {Injectable} from "@nestjs/common";
-import {ExtractJwt, Strategy} from "passport-jwt";
+import { PassportStrategy } from "@nestjs/passport"
+import { Injectable } from "@nestjs/common"
+import { ExtractJwt, Strategy } from "passport-jwt"
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy){
-    constructor() {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration:false,
-            secretOrKey: process.env.JWT_SECRET
-        });
-    }
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor () {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET
+    })
+  }
 
-    async validate(payload: any){
-        return {
-            userID:payload.userID
-        };
+  async validate (payload: any) {
+    return {
+      userID: payload.userID
     }
+  }
 }
