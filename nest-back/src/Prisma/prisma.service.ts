b38b1f2ba292a783ea8a7c263b6cc2f7f906df84
slickjumper <u19050412@tuks.co.za>
2021-07-07 16:13:28 +0200

    service lint and new prisma schema clean up - Tinashe Chamisa

--- nest-back/src/Prisma/prisma.service.ts
+++ nest-back/src/Prisma/prisma.service.ts
@@ -1,14 +1,14 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common"
+import { PrismaClient } from "@prisma/client"
 
 @Injectable()
 export class PrismaService extends PrismaClient
-    implements OnModuleInit, OnModuleDestroy {
-    async onModuleInit() {
-        await this.$connect();
-    }
+  implements OnModuleInit, OnModuleDestroy {
+  async onModuleInit () {
+    await this.$connect()
+  }
 
-    async onModuleDestroy() {
-        await this.$disconnect();
-    }
+  async onModuleDestroy () {
+    await this.$disconnect()
+  }
 }
--- nest-back/src/app.module.ts
+++ nest-back/src/app.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
-import {WorkoutModule} from "./Workout/workout.module";
-import { UserModule } from './User/user.module';
-import {GoogleStrategy} from "./User/PassportStrategies/google.strategy";
+import { Module } from "@nestjs/common"
+import { WorkoutModule } from "./Workout/workout.module"
+import { UserModule } from "./User/user.module"
+import { GoogleStrategy } from "./User/PassportStrategies/google.strategy"
 
 @Module({
   imports: [WorkoutModule, UserModule],
-  providers: [GoogleStrategy],
+  providers: [GoogleStrategy]
 
 })
 export class AppModule {}
