a108be408ee9fbcaf6a09e37cc5951a68c6dde6c
slickjumper <u19050412@tuks.co.za>
2021-07-07 16:12:45 +0200

    unit test lint and new prisma schema clean up - Tinashe Chamisa

--- nest-back/test/Workout/UnitTests/getWorkouts.spec.ts
+++ nest-back/test/Workout/UnitTests/getWorkouts.spec.ts
@@ -1,34 +1,31 @@
-import { MockContext, Context, createMockContext } from "../../../context";
-import {WorkoutService} from "../../../src/Workout/workout.service";
-import {v4 as uuidv4 } from 'uuid';
-import {PrismaClient} from "@prisma/client/scripts/default-index";
+import { MockContext, Context, createMockContext } from "../../../context"
+import { WorkoutService } from "../../../src/Workout/workout.service"
+import { v4 as uuidv4 } from "uuid"
+import { PrismaClient } from "@prisma/client/scripts/default-index"
 
 let mockCtx: MockContext
 let ctx: Context
 let workoutService: WorkoutService
 let prisma: PrismaClient
 
-describe('Unit tests of the getWorkouts function in the Workout Service', () => {
+describe("Unit tests of the getWorkouts function in the Workout Service", () => {
+  beforeEach(() => {
+    workoutService = new WorkoutService(prisma)
+    mockCtx = createMockContext()
+    ctx = (mockCtx as unknown) as Context
+  })
 
-    beforeEach(() => {
-        workoutService = new WorkoutService(prisma);
-        mockCtx = createMockContext()
-        ctx = (mockCtx as unknown) as Context
-    })
+  test("Should receive valid information about all workouts", async () => {
+    const workout = [{
+      workoutID: uuidv4(),
+      workoutTitle: "test",
+      workoutDescription: "test",
+      plannerID: uuidv4()
+    }]
+    mockCtx.prisma.workout.findMany.mockResolvedValue(workout)
 
-    test('Should receive valid information about all workouts', async () => {
-        const workout = [{
-            workoutID: uuidv4(),
-            workoutTitle: "test",
-            workoutDescription: "test",
-            tags: [],
-            planner_ID: uuidv4()
-        }]
-        mockCtx.prisma.workout.findMany.mockResolvedValue(workout)
+    const response = await workoutService.getWorkouts(ctx)
 
-        const response = await workoutService.getWorkouts(ctx)
-
-        expect(response).toBe(workout);
-    })
-
+    expect(response).toBe(workout)
+  })
 })
