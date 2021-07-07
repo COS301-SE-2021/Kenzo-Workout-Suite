5d86df204f6681427b0b96ac151139c74d4f77f8
slickjumper <u19050412@tuks.co.za>
2021-07-07 16:13:10 +0200

    integration test lint and new prisma schema clean up - Tinashe Chamisa

--- nest-back/test/Workout/IntegrationTests/updateExercise.spec.ts
+++ nest-back/test/Workout/IntegrationTests/updateExercise.spec.ts
@@ -1,67 +1,65 @@
-import {MockContext, Context, createMockContext, ActualPrisma} from "../../../context";
-import {WorkoutService} from "../../../src/Workout/workout.service";
-import {v4 as uuidv4 } from 'uuid';
-import {PrismaClient} from "@prisma/client/scripts/default-index";
-import {Tag} from "@prisma/client";
+import { MockContext, Context, createMockContext, ActualPrisma } from "../../../context"
+import { WorkoutService } from "../../../src/Workout/workout.service"
+import { v4 as uuidv4 } from "uuid"
+import { PrismaClient } from "@prisma/client/scripts/default-index"
+import { Tag } from "@prisma/client"
 
 let ctx: Context
 let workoutService: WorkoutService
 let prisma: PrismaClient
 
-const uuidExercise = uuidv4();
-const uuidPlanner = uuidv4();
+const uuidExercise = uuidv4()
+const uuidPlanner = uuidv4()
 
-describe('Integration tests of the updateExercise function in the Workout Service', () => {
+describe("Integration tests of the updateExercise function in the Workout Service", () => {
+  beforeEach(async () => {
+    workoutService = new WorkoutService(prisma)
+    ctx = ActualPrisma()
+    await ctx.prisma.exercise.deleteMany()
+    await ctx.prisma.user.deleteMany()
+    await ctx.prisma.user.create({
+      data: {
+        userId: uuidPlanner,
+        email: "test&gmail.com",
+        firstName: "test",
+        lastName: "test",
+        password: "Test123!",
+        dateOfBirth: null,
+        userType: "PLANNER"
+      }
+    })
+  })
 
-    beforeEach(async () => {
-        workoutService = new WorkoutService(prisma);
-        ctx = ActualPrisma()
-        await ctx.prisma.exercise.deleteMany();
-        await ctx.prisma.user.deleteMany();
-        await ctx.prisma.user.create({
-            data: {
-                userId: uuidPlanner,
-                email: "test&gmail.com",
-                firstName: "test",
-                lastName: "test",
-                password: "Test123!",
-                dateOfBirth: null,
-                userType: "PLANNER",
-            }
-        });
+  test("Valid exercise passed in without tags, should receive successful message", async () => {
+    await ctx.prisma.exercise.create({
+      data: {
+        exercise: uuidExercise,
+        title: "test",
+        description: "test",
+        repRange: "test",
+        sets: 4,
+        Posedescription: "test",
+        restPeriod: 2,
+        duratime: 2,
+        planner_ID: uuidPlanner
+      }
     })
 
-    test('Valid exercise passed in without tags, should receive successful message', async () => {
-        await ctx.prisma.exercise.create({
-            data: {
-                exercise: uuidExercise,
-                title: "test",
-                description: "test",
-                repRange: "test",
-                sets: 4,
-                Posedescription: "test",
-                restPeriod: 2,
-                duratime: 2,
-                planner_ID: uuidPlanner
-            }
-        });
+    const emptyTag: Tag[] = []
 
-        let emptyTag: Tag[] = [];
+    const response = await workoutService.updateExercise(uuidExercise, "test", "test", "test", 4, "test", 2, emptyTag, 2, uuidPlanner, ctx)
+    expect(response).toStrictEqual("Exercise updated.")
+  })
 
-        const response = await workoutService.updateExercise(uuidExercise, 'test', 'test', 'test', 4, 'test', 2, emptyTag, 2, uuidPlanner, ctx)
-        expect(response).toStrictEqual("Exercise updated.");
-    })
+  test("Null exercise passed in, should throw PreconditionFailedException", async () => {
+    await expect(workoutService.updateExercise("", "", "", "", 0, "", 0, [], 0, "", ctx)).rejects.toThrow("Invalid exercise object passed in.")
+  })
 
-    test('Null exercise passed in, should throw PreconditionFailedException', async () => {
-        await expect(workoutService.updateExercise('', '', '', '', 0, '', 0, [], 0, "", ctx)).rejects.toThrow("Invalid exercise object passed in.")
-    })
+  test("Incomplete exercise passed in, should throw PreconditionFailedException", async () => {
+    await expect(workoutService.updateExercise("test", "", "test", "", 0, "test", 0, [], 0, "", ctx)).rejects.toThrow("Invalid exercise object passed in.")
+  })
 
-    test('Incomplete exercise passed in, should throw PreconditionFailedException', async () => {
-        await expect(workoutService.updateExercise('test', '', 'test', '', 0, 'test', 0, [], 0, "", ctx)).rejects.toThrow("Invalid exercise object passed in.")
-    })
-
-    test('Nonexistent exercise, should throw NotFoundException', async () => {
-        await expect(workoutService.updateExercise('test', 'test', 'test', 'test', 0, 'test', 0, [], 0, "", ctx)).rejects.toThrow("Invalid exercise object passed in.")
-    })
-
+  test("Nonexistent exercise, should throw NotFoundException", async () => {
+    await expect(workoutService.updateExercise("test", "test", "test", "test", 0, "test", 0, [], 0, "", ctx)).rejects.toThrow("Invalid exercise object passed in.")
+  })
 })
