4e31b8fc31d9e32022683e391b39d1e1ed001aa0
Luca Azmanov <72404660+Aztrixs@users.noreply.github.com>
2021-08-12 21:08:28 +0200

    Fixed client list testing - Luca Azmanov

--- front-end/src/app/client-list/client-list.page.spec.ts
+++ front-end/src/app/client-list/client-list.page.spec.ts
@@ -2,16 +2,20 @@ import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { IonicModule } from "@ionic/angular";
 
 import { ClientListPage } from "./client-list.page";
+import {Router} from "@angular/router";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe("ClientListPage", () => {
     let component: ClientListPage;
     let fixture: ComponentFixture<ClientListPage>;
+    let routeMock: Router;
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [ ClientListPage ],
-            imports: [IonicModule.forRoot()]
+            imports: [IonicModule.forRoot(), RouterTestingModule]
         }).compileComponents();
+        routeMock = TestBed.inject(Router);
 
         fixture = TestBed.createComponent(ClientListPage);
         component = fixture.componentInstance;
