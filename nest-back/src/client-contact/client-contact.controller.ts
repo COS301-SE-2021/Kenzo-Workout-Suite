9df801f3ea2ae5a6b7e6c18179c8bafbbd27bb16
Zeee2000 <u19086688@tuks.co.za>
2021-08-14 17:45:13 +0200

    Partial contact implementation -Zelealem Tesema

--- nest-back/src/client-contact/client-contact.controller.ts
+++ nest-back/src/client-contact/client-contact.controller.ts
@@ -1,14 +1,138 @@
-import { Controller, Get } from "@nestjs/common"
+import { Body, Controller, Delete, Get, Post, Put, Request, UseGuards } from "@nestjs/common"
 import { ClientContactService } from "./client-contact.service"
+import { ApiBadRequestResponse, ApiBody, ApiInternalServerErrorResponse, ApiOkResponse } from "@nestjs/swagger"
+import { createContactDTO, deleteContactDTO, updateContactDTO } from "../Workout/workout.model"
+import { JwtAuthGuard } from "../User/AuthGuards/jwt-auth.guard"
+import { ActualPrisma, Context } from "../../context"
 
 @Controller("client-contact")
 export class ClientContactController {
+  ctx: Context
+
   constructor (private readonly clientContactService:ClientContactService) {
+    this.ctx = ActualPrisma()
   }
 
-    @Get("test")
+    @Get("email")
   testFunction (
   ) {
-    return this.clientContactService.doSomething()
+    return this.clientContactService.sendEmailToContact()
+  }
+
+  /**
+   *Workout Controller - Create Client Contact
+   *
+   * @throws BadRequestException if:
+   * @return Object array of all contact objects
+   * @author Msi Sibanyoni & Tinashe Chamisa
+   *
+   */
+  @Get("getAllClientContacts")
+  @ApiOkResponse({
+    description: "An array of client contact objects."
+  })
+  @ApiBadRequestResponse({
+    description: "Could not retrieve client contact objects"
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Internal server error."
+  })
+    getAllClientContacts (
+    ) {
+      return this.clientContactService.getAllClientContacts(this.ctx)
+    }
+
+  /**
+   *Workout Controller - Delete Client Contact
+   *
+   * @param contactEmail - Email to delete contact.
+   * @throws BadRequestException if:
+   * @return Message indicating success.
+   *
+   * @author Msi Sibanyoni & Tinashe Chamisa
+   *
+   */
+  @Delete("deleteClientContact")
+  @ApiOkResponse({
+    description: "Client deleted."
+  })
+  @ApiBadRequestResponse({
+    description: "Could not delete client."
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Internal server error."
+  })
+  @ApiBody({ type: deleteContactDTO })
+  deleteClientContact (
+      @Body("contactEmail") contactEmail: string
+  ) {
+    return this.clientContactService.deleteClientContact(contactEmail, this.ctx)
+  }
+
+  /**
+   *Workout Controller - Create Client Contact
+   *
+   * @param contactEmail - Email to create contact.
+   * @param name - Name to create contact.
+   * @param surname - Surname to create contact.
+   * @param req - Details of user who made request
+   * @throws BadRequestException if:
+   * @return Message indicating success.
+   *
+   * @author Msi Sibanyoni & Tinashe Chamisa
+   *
+   */
+  @UseGuards(JwtAuthGuard)
+  @Post("createClientContact")
+  @ApiOkResponse({
+    description: "Client Created."
+  })
+  @ApiBadRequestResponse({
+    description: "Could not create client."
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Internal server error."
+  })
+  @ApiBody({ type: createContactDTO })
+  createClientContact (
+      @Body("contactEmail") contactEmail: string,
+      @Body("name") name: string,
+      @Body("surname") surname: string,
+      @Request() req
+  ) {
+    return this.clientContactService.createClientContact(contactEmail, name, surname, req.user.userID, this.ctx)
+  }
+
+  /**
+   *Workout Controller - Update Client Contact
+   *
+   * @param contactEmail - Email to update contact.
+   * @param name - Name to update contact.
+   * @param surname - Surname to update contact.
+   * @throws BadRequestException if:
+   * @return Message indicating success.
+   *
+   * @author Msi Sibanyoni & Tinashe Chamisa
+   *
+   */
+  @UseGuards(JwtAuthGuard)
+  @Put("updateClientContact")
+  @ApiOkResponse({
+    description: "Client updated."
+  })
+  @ApiBadRequestResponse({
+    description: "Could not update client."
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Internal server error."
+  })
+  @ApiBody({ type: updateContactDTO })
+  updateClientContact (
+      @Body("contactEmail") contactEmail: string,
+      @Body("name") name: string,
+      @Body("surname") surname: string,
+      @Request() req
+  ) {
+    return this.clientContactService.updateClientContact(contactEmail, name, surname, req.user.userID, this.ctx)
   }
 }
--- nest-back/src/client-contact/client-contact.service.ts
+++ nest-back/src/client-contact/client-contact.service.ts
@@ -8,108 +8,121 @@ export class ClientContactService {
 
   }
 
-  async createClientContact (contactEmail: string, name: string, surname: string, plannerID:string, ctx: Context) {
-    if (contactEmail === "" || name === "" || surname === "" || contactEmail == null || name === null || surname == null) {
-      throw new NotFoundException("Parameters can not be left empty!")
-    }
-    try {
-      await ctx.prisma.contacts.create({
-        data: {
-          contactEmail: contactEmail,
-          name: name,
-          surname: surname,
-          planner: {
-            connect: {
-              userID: plannerID
-            }
-          }
-        }
-      })
-      return "Client contact created."
-    } catch (e) {
-      throw new BadRequestException(e)
-    }
-  }
+  // async createClientContact (contactEmail: string, name: string, surname: string, plannerID:string, ctx: Context) {
+  //   if (contactEmail === "" || name === "" || surname === "" || contactEmail == null || name === null || surname == null) {
+  //     throw new NotFoundException("Parameters can not be left empty!")
+  //   }
+  //   try {
+  //     await ctx.prisma.contacts.create({
+  //       data: {
+  //         contactEmail: contactEmail,
+  //         name: name,
+  //         surname: surname,
+  //         planner: {
+  //           connect: {
+  //             userID: plannerID
+  //           }
+  //         }
+  //       }
+  //     })
+  //     return "Client contact created."
+  //   } catch (e) {
+  //     throw new BadRequestException(e)
+  //   }
+  // }
+  //
+  // async updateClientContact (contactEmail: string, newContactEmail: string, newName: string, newSurname: string, plannerID:string, ctx: Context) {
+  //   if (contactEmail === "" || newName === "" || newSurname === "" || contactEmail == null || newName === null || newSurname == null) {
+  //     throw new NotFoundException("Parameters can not be left empty!")
+  //   }
+  //   try {
+  //     await ctx.prisma.contacts.update({
+  //       where: {
+  //         contactEmail: contactEmail
+  //       },
+  //       data: {
+  //         contactEmail: newContactEmail,
+  //         name: newName,
+  //         surname: newSurname,
+  //         planner: {
+  //           connect: {
+  //             userID: plannerID
+  //           }
+  //         }
+  //       }
+  //     })
+  //     return "Client contact updated."
+  //   } catch (e) {
+  //     throw new BadRequestException(e)
+  //   }
+  // }
+  //
+  // async deleteClientContact (contactEmail: string, plannerID:String, ctx: Context) {
+  //   if (contactEmail === "" || contactEmail == null) {
+  //     throw new NotFoundException("Parameters can not be left empty!")
+  //   }
+  //   try {
+  //     await ctx.prisma.contacts.update({
+  //       where: {
+  //         contactEmail: contactEmail
+  //       },
+  //       data: {
+  //         planners: {
+  //           disconnect: {
+  //             userID: plannerID
+  //           }
+  //         }
+  //       }
+  //     })
+  //     return "Client contact deleted."
+  //   } catch (e) {
+  //     throw new BadRequestException(e)
+  //   }
+  // }
+  //
+  // async getClientContactDetails (contactEmail: string, ctx: Context) {
+  //   const clientContacts = await ctx.prisma.contacts.findUnique({ // search for workouts that meet the requirement
+  //     where: {
+  //       contactEmail: contactEmail
+  //     },
+  //     select: {
+  //       contactEmail: true,
+  //       name: true,
+  //       surname: true
+  //     }
+  //   })
+  //   if (clientContacts == null) {
+  //     throw new BadRequestException("No client contact with that email found.")
+  //   } else {
+  //     return clientContacts
+  //   }
+  // }
+  //
+  // async getAllClientContacts (ctx: Context) {
+  //   const clientContacts = await ctx.prisma.contacts.findMany({ // search for workouts that meet the requirement
+  //     select: {
+  //       contactEmail: true,
+  //       name: true,
+  //       surname: true
+  //     }
+  //   })
+  //   if (clientContacts == null) {
+  //     throw new BadRequestException("No client contacts found.")
+  //   } else {
+  //     return clientContacts
+  //   }
+  // }
 
-  async updateClientContact (contactEmail: string, newContactEmail: string, newName: string, newSurname: string, plannerID:string, ctx: Context) {
-    if (contactEmail === "" || newName === "" || newSurname === "" || contactEmail == null || newName === null || newSurname == null) {
-      throw new NotFoundException("Parameters can not be left empty!")
+  async sendEmailToContact () {
+    const sgMail = require("@sendgrid/mail")
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+      to: "zelutesema@gmail.com",
+      from: "crackedstudios.up@gmail.com",
+      subject: "Sending with SendGrid is Fun",
+      text: "and easy to do anywhere, even with Node.js",
+      html: "<strong>and easy to do anywhere, even with Node.js</strong>"
     }
-    try {
-      await ctx.prisma.contacts.update({
-        where: {
-          contactEmail: contactEmail
-        },
-        data: {
-          contactEmail: newContactEmail,
-          name: newName,
-          surname: newSurname,
-          planner: {
-            connect: {
-              userID: plannerID
-            }
-          }
-        }
-      })
-      return "Client contact updated."
-    } catch (e) {
-      throw new BadRequestException(e)
-    }
-  }
-
-  async deleteClientContact (contactEmail: string, plannerID:String, ctx: Context) {
-    if (contactEmail === "" || contactEmail == null) {
-      throw new NotFoundException("Parameters can not be left empty!")
-    }
-    try {
-      await ctx.prisma.contacts.update({
-        where: {
-          contactEmail: contactEmail
-        },
-        data: {
-          planners: {
-            disconnect: {
-              userID: plannerID
-            }
-          }
-        }
-      })
-      return "Client contact deleted."
-    } catch (e) {
-      throw new BadRequestException(e)
-    }
-  }
-
-  async getClientContactDetails (contactEmail: string, ctx: Context) {
-    const clientContacts = await ctx.prisma.contacts.findUnique({ // search for workouts that meet the requirement
-      where: {
-        contactEmail: contactEmail
-      },
-      select: {
-        contactEmail: true,
-        name: true,
-        surname: true
-      }
-    })
-    if (clientContacts == null) {
-      throw new BadRequestException("No client contact with that email found.")
-    } else {
-      return clientContacts
-    }
-  }
-
-  async getAllClientContacts (ctx: Context) {
-    const clientContacts = await ctx.prisma.contacts.findMany({ // search for workouts that meet the requirement
-      select: {
-        contactEmail: true,
-        name: true,
-        surname: true
-      }
-    })
-    if (clientContacts == null) {
-      throw new BadRequestException("No client contacts found.")
-    } else {
-      return clientContacts
-    }
+    sgMail.send(msg)
   }
 }
