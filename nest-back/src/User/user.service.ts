f4de04a4e85ea544e1a6da898762acba59d31d03
Zeee2000 <u19086688@tuks.co.za>
2021-09-02 22:22:36 +0200

    Edited user service - Zelealem Tesema

--- nest-back/src/User/user.service.ts
+++ nest-back/src/User/user.service.ts
@@ -9,8 +9,6 @@ import { JwtService } from "@nestjs/jwt"
 import { User } from "@prisma/client"
 import * as bcrypt from "bcrypt"
 import { Context } from "../../context"
-import { initializeApp } from "firebase-admin/lib/firebase-namespace-api"
-import { log } from "util"
 
 @Injectable()
 export class UserService {
@@ -24,22 +22,26 @@ export class UserService {
     measurementId: process.env.MEASUREMENTID
   };
 
-  constructor (private jwtService: JwtService) {}
+  private admin;
+  constructor (private jwtService: JwtService) {
+    this.admin = require("firebase-admin")
+    if (!this.admin.apps.length) { this.admin.initializeApp(this.firebaseConfig) }
+  }
 
   /**
-     *User Service - Validate User
-     *
-     * @param email This is the email address of the User to be validated
-     * @param pass This is the password of the User that is to be validated
-     * @param ctx  This is the prisma context that is injected into the function
-     * @throws NotFoundException if:
-     *                               -An empty email address is passed in.
-     *                               -An empty password is passed in
-     *                               -Invalid combination of email address and password
-     * @return  Promise User object without the password.
-     * @author Zelealem Tesema
-     *
-     */
+   *User Service - Validate User
+   *
+   * @param email This is the email address of the User to be validated
+   * @param pass This is the password of the User that is to be validated
+   * @param ctx  This is the prisma context that is injected into the function
+   * @throws NotFoundException if:
+   *                               -An empty email address is passed in.
+   *                               -An empty password is passed in
+   *                               -Invalid combination of email address and password
+   * @return  Promise User object without the password.
+   * @author Zelealem Tesema
+   *
+   */
   async validateUser (email: string, pass: string, ctx: Context): Promise<any> {
     if (email == null || pass == null) {
       throw new NotFoundException("Invalid Email or Password")
@@ -70,16 +72,16 @@ export class UserService {
   }
 
   /**
-     * User Service- login
-     * @param user This is the User object with the email address and password that need to be validated.
-     * @throws NotFoundException if:
-     *                          -An empty User object is passed into the function
-     *                          -A User object without the field 'userId' is passed into the function
-     *
-     * @return Promise that consists of the access token
-     *
-     * @author Zelealem Tesema
-     */
+   * User Service- login
+   * @param user This is the User object with the email address and password that need to be validated.
+   * @throws NotFoundException if:
+   *                          -An empty User object is passed into the function
+   *                          -A User object without the field 'userId' is passed into the function
+   *
+   * @return Promise that consists of the access token
+   *
+   * @author Zelealem Tesema
+   */
   async login (user: any) {
     if (user == null) {
       throw new NotFoundException("Invalid User object passed in.")
@@ -97,29 +99,29 @@ export class UserService {
   }
 
   /**
-     *User Service - Sign Up
-     *
-     * @param user This is the User object that consists of the following details:
-     *              -firstName
-     *              -lastName
-     *              -email
-     *              -userType
-     *              -password
-     *
-     * @param ctx
-     *
-     * @throws PreconditionFailedException if:
-     *                          -Empty/null User object is passed into the function
-     *                          -Email address that does not conform to the email address standard is passed in
-     *                          -Password that does not conform to the password standard is passed in (atleast 8 characters consisting of one lower case, one upper case, one number and one special character)
-     *
-     *@throws BadRequestException if:
-     *                          -A User with the passed in Users email already exists in the database.
-     *
-     *@return Promise This promise consists the JWT access token.
-     *@author Zelealem Tesema
-     *
-     */
+   *User Service - Sign Up
+   *
+   * @param user This is the User object that consists of the following details:
+   *              -firstName
+   *              -lastName
+   *              -email
+   *              -userType
+   *              -password
+   *
+   * @param ctx
+   *
+   * @throws PreconditionFailedException if:
+   *                          -Empty/null User object is passed into the function
+   *                          -Email address that does not conform to the email address standard is passed in
+   *                          -Password that does not conform to the password standard is passed in (atleast 8 characters consisting of one lower case, one upper case, one number and one special character)
+   *
+   *@throws BadRequestException if:
+   *                          -A User with the passed in Users email already exists in the database.
+   *
+   *@return Promise This promise consists the JWT access token.
+   *@author Zelealem Tesema
+   *
+   */
   async signUp (user:User, ctx: Context) : Promise<any> {
     if (user == null) {
       throw new PreconditionFailedException("Invalid User object")
@@ -166,22 +168,22 @@ export class UserService {
   }
 
   /**
-     * User Service - findUserByUUID
-     *
-     * @param passedUserId This is the ID for which the User details are being searched for.
-     * @param ctx This is the prisma context that can be mocked.
-     *
-     * @throws BadRequestException if:
-     *                             -An empty or null User ID is passed into the function
-     *                             -The database fails to retrieve the User details
-     *
-     *@throws NotFoundException if:
-     *                          -No User with such UUID exists.
-     *
-     *@return Promise This returns the details of the User.
-     *
-     * @author Zelealem Tesema
-     */
+   * User Service - findUserByUUID
+   *
+   * @param passedUserId This is the ID for which the User details are being searched for.
+   * @param ctx This is the prisma context that can be mocked.
+   *
+   * @throws BadRequestException if:
+   *                             -An empty or null User ID is passed into the function
+   *                             -The database fails to retrieve the User details
+   *
+   *@throws NotFoundException if:
+   *                          -No User with such UUID exists.
+   *
+   *@return Promise This returns the details of the User.
+   *
+   * @author Zelealem Tesema
+   */
   async findUserByUUID (passedUserId: string, ctx: Context) : Promise<any> {
     if (passedUserId === null || passedUserId === "") {
       throw new BadRequestException("Null values cannot be passed in for userId")
@@ -206,21 +208,21 @@ export class UserService {
   }
 
   /**
-     * User Service - Update User details
-     *
-     * @param firstName This is the first name of the User that is required to be updated
-     * @param lastName  This is the last name of the User that is required to be updated.
-     * @param dateOfBirth  This is the date of birth of the User that is required to be updated.
-     * @param userID    This is the User ID of the User that is required to be updated. ( This UserID should be passed in through the bearer token Authorisation header)
-     * @param ctx   This is the prisma context that can be passed in as a mock
-     *
-     * @throws BadRequestException if:
-     *                          -Null values are passed in for the firstName, LastName or userID
-     *                          -Empty values are passed in for the firstName, LastName or userID
-     *                          -The User details could not be updated
-     *
-     * @author Zelealem Tesema
-     */
+   * User Service - Update User details
+   *
+   * @param firstName This is the first name of the User that is required to be updated
+   * @param lastName  This is the last name of the User that is required to be updated.
+   * @param dateOfBirth  This is the date of birth of the User that is required to be updated.
+   * @param userID    This is the User ID of the User that is required to be updated. ( This UserID should be passed in through the bearer token Authorisation header)
+   * @param ctx   This is the prisma context that can be passed in as a mock
+   *
+   * @throws BadRequestException if:
+   *                          -Null values are passed in for the firstName, LastName or userID
+   *                          -Empty values are passed in for the firstName, LastName or userID
+   *                          -The User details could not be updated
+   *
+   * @author Zelealem Tesema
+   */
   async updateUserDetails (firstName:string, lastName:string, dateOfBirth:string, userId:string, ctx:Context) {
     if (firstName == null || lastName == null || userId == null || firstName === "" || lastName === "" || userId === "") {
       throw new BadRequestException("Null values can not be passed in for firstName, lastName or userId")
@@ -272,12 +274,8 @@ export class UserService {
       throw new BadRequestException("No such google User")
     }
 
-    const admin = require("firebase-admin")
-
-    admin.initializeApp(this.firebaseConfig)
-
     try {
-      await admin.auth().verifyIdToken(accessToken)
+      await this.admin.auth().verifyIdToken(accessToken)
     } catch (err) {
       throw new UnauthorizedException("Invalid credentials")
     }
@@ -331,39 +329,39 @@ export class UserService {
   }
 
   /**
-     * User Service- validateEmail
-     *
-     * @param email This is the email address that needs to be validated within the function
-     *
-     * @return true if:
-     *              - The email conforms to the email standards.
-     *
-     * @return false if:
-     *              - The email address does not conform to the email standards.
-     *
-     * @author Zelealem Tesema
-     */
+   * User Service- validateEmail
+   *
+   * @param email This is the email address that needs to be validated within the function
+   *
+   * @return true if:
+   *              - The email conforms to the email standards.
+   *
+   * @return false if:
+   *              - The email address does not conform to the email standards.
+   *
+   * @author Zelealem Tesema
+   */
   validateEmail (email:string) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     return re.test(String(email).toLowerCase())
   }
 
   /**
-     * User Service- validatePassword
-     *
-     * @param password
-     *
-     * @return true if:
-     *          - The password consists of atleast 8 characters, of which atleast one character is a lowercase alphabet, one character is an uppercase alphabet, one character is a digit and one is a special character
-     *
-     * @return false if:
-     *          -Passsword does not consist of a lower case character
-     *          -Password does not consist of an upper case character
-     *          -Password does not consist of a special character
-     *          -Password is shorter than 8 characters
-     *
-     *@author Zelealem Tesema
-     */
+   * User Service- validatePassword
+   *
+   * @param password
+   *
+   * @return true if:
+   *          - The password consists of atleast 8 characters, of which atleast one character is a lowercase alphabet, one character is an uppercase alphabet, one character is a digit and one is a special character
+   *
+   * @return false if:
+   *          -Passsword does not consist of a lower case character
+   *          -Password does not consist of an upper case character
+   *          -Password does not consist of a special character
+   *          -Password is shorter than 8 characters
+   *
+   *@author Zelealem Tesema
+   */
   validatePassword (password:string) {
     const re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/
     return re.test(password)
